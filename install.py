81d6796ae9c277d4745711aa20574e347352b38c
Sebastian Blask <blask@gmx.de>
2014-02-09 21:04:21 +0100

    Move back to modified .bashrc
     - history size settings weren't taken into account
     - PS1 caused problems with terminal title

--- install.py
+++ install.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import argparse
 import os
 import sys
 
@@ -35,38 +34,6 @@ def create_link(relative_path, from_file_name, to_file_name):
     os.symlink(to_path, from_path)
 
 
-def in_bashrc(line):
-    with open(os.path.join(HOME_DIRECTORY, '.bashrc')) as file_object:
-        if line in file_object.readlines():
-            return True
-
-
-def add_to_bashrc(line):
-    with open(os.path.join(HOME_DIRECTORY, '.bashrc'), 'a') as file_object:
-        file_object.write('\n' + line + '\n')
-
-
-def ensure_reference(entry_file):
-    inclusion = 'if [ -f ~/%s ]; then . ~/%s; fi\n' % (entry_file, entry_file)
-    if not in_bashrc(inclusion):
-        add_to_bashrc(inclusion)
-
-
-def get_entry_file():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'entry_file',
-        help='The file in this directory whose link should be referenced in'
-             ' .bashrc',
-        nargs='?',
-        default='bashrc_custom.dotfile',
-    )
-    arguments = parser.parse_args()
-    if not os.path.exists(os.path.join(BASE_DIRECTORY, arguments.entry_file)):
-        raise Exception('No entry file given')
-    return get_link_file_name(arguments.entry_file)
-
-
 def get_link_file_name(input_file_name):
     file_name, file_extension = os.path.splitext(input_file_name)
     if file_extension == SYMLINK_EXTENSION:
@@ -82,17 +49,15 @@ def main():
     ends with SYMLINK_EXTENSION or DOTFILE_EXTENSION.
     Also make sure that given file is referenced in .bashrc
     '''
-    entry_file = get_entry_file()
     for relative_path, directories, files in os.walk(BASE_DIRECTORY):
         if SYMLINK_EXTENSION in relative_path or \
            DOTFILE_EXTENSION in relative_path:
-            # has been handled earlier
+            # has been handled earlier (don't support nesting)
             continue
         for thing in directories + files:
             link_file_name = get_link_file_name(thing)
             if link_file_name:
                 create_link(relative_path, link_file_name, thing)
-    ensure_reference(entry_file)
 
 
 if __name__ == '__main__':
