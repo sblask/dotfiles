d042f8358a4ee9e5dcb0391de4b848dd93bafc25
Sebastian Blask <blask@gmx.de>
2014-02-10 15:41:30 +0100

    Add more boilerplate to python skeleton

--- skeletons.dotfile/skeleton.py
+++ skeletons.dotfile/skeleton.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-n',
+        '--name',
+        help='',
+    )
+    return parser.parse_args()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
 
