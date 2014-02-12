3fea38d6e87989d1b0879fae6dfa5b68aa23552a
Sebastian Blask <blask@gmx.de>
2014-02-12 15:58:44 +0100

    Exclude pack script and patch from archive

--- pack.sh
+++ pack.sh
@@ -9,5 +9,5 @@ git clone git@github.com:sblask/scm_breeze.git $SCM_BREEZE_CLONE_DIRECTORY
 patch -p0 --directory=$SCM_BREEZE_CLONE_DIRECTORY < scm_breeze_patch
 
 EXCLUDED_LINKS=$(find . -type l | tr "\\n" "|" | sed "s/|$//" | sed "s/|/ --exclude /g")
-tar cvzf $DOTFILES_DIRECTORY/dotfiles.tgz $DOTFILES_DIRECTORY/ --exclude-vcs --exclude dotfiles.tgz --exclude $EXCLUDED_LINKS --transform 's/^./dotfiles/'
+tar cvzf $DOTFILES_DIRECTORY/dotfiles.tgz $DOTFILES_DIRECTORY/ --exclude-vcs --exclude dotfiles.tgz --exclude pack.sh --exclude scm_breeze_patch --exclude $EXCLUDED_LINKS --transform 's/^./dotfiles/'
 
