83ed3937f59d864f4710188adfd79d87981657ef
Sebastian Blask <blask@gmx.de>
2014-07-07 13:24:08 +0200

    Stay away from existing tmux sessions

--- bin.dotfile/cssh-handler.sh
+++ bin.dotfile/cssh-handler.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# handle ssh:// protocol links
-xterm -e "~/.bin/tmux-cssh $(echo $1 | cut -c 8- | sed 's/,/ /g')"
+
+SESSION_NAME="ssh_$(echo $SERVERS | awk '{split($0,a,"-"); printf "%s_%s", a[1], a[2]}')"
+
+if tmux has-session -t $SESSION_NAME; then
+    notify-send "Session $SESSION_NAME exists already" --expire-time=3000
+else
+    # handle cssh:// protocol links
+    xterm -e "~/.bin/tmux-cssh $(echo $1 | cut -c 8- | sed 's/,/ /g')"
+fi
 
