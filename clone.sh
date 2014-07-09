428340e36dfa2e4e0b031bc09b5573ee8382ae77
Sebastian Blask <blask@gmx.de>
2014-07-09 02:14:06 +0200

    Fix clone script

--- clone.sh
+++ clone.sh
@@ -12,6 +12,6 @@ git clone https://github.com/sblask/scm_breeze.git $SCM_BREEZE_CLONE_DIRECTORY
 patch -p0 --directory=$SCM_BREEZE_CLONE_DIRECTORY < scm_breeze_patch
 
 FONTS_CLONE_DIRECTORY=$DOTFILES_DIRECTORY/.fonts
-mkdir -p FONTS_CLONE_DIRECTORY
+mkdir -p $FONTS_CLONE_DIRECTORY
 wget https://github.com/Lokaltog/powerline-fonts/raw/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf -O $FONTS_CLONE_DIRECTORY/DejaVu\ Sans\ Mono\ for\ Powerline.ttf.symlink
 
