4132c711ae4f8ab85e926d30330db5b277c09938
Sebastian Blask <blask@gmx.de>
2014-07-24 13:09:46 +0200

    cssh is not using separate tmux session anymore

--- bin.dotfile/cssh-handler.sh
+++ bin.dotfile/cssh-handler.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-SESSION_NAME="ssh_$(echo $SERVERS | awk '{split($0,a,"-"); printf "%s_%s", a[1], a[2]}')"
-
-if tmux has-session -t $SESSION_NAME; then
-    notify-send "Session $SESSION_NAME exists already" --expire-time=3000
-else
-    # handle cssh:// protocol links
-    xterm -e "~/.bin/tmux-cssh $(echo $1 | cut -c 8- | sed 's/,/ /g')"
-fi
+# handle cssh:// protocol links
+xterm -e "~/.bin/tmux-cssh $(echo $1 | cut -c 8- | sed 's/,/ /g')"
 
