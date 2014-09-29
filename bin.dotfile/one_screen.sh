0408c1e0534e62c9a3b2967b6f0f774ab5b6092a
Sebastian Blask <blask@gmx.de>
2014-09-29 15:07:03 +0200

    Add screen layout scripts

---
+++ bin.dotfile/one_screen.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr --output DP2 --off
+xrandr --output LVDS1 --mode 1366x768 --pos 0x0
+xfconf-query -c xfce4-panel -p /panels/panel-0/output-name -s LVDS1
+
---
+++ bin.dotfile/two_screens.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr --output DP2 --mode 2560x1080 --pos 0x0
+xrandr --output LVDS1 --mode 1366x768 --pos 2560x1080
+xfconf-query -c xfce4-panel -p /panels/panel-0/output-name -s DP2
+
