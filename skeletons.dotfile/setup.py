67425dd66a5d74762d95372619fce78362a85625
Sebastian Blask <blask@gmx.de>
2015-10-19 16:59:32 +0200

    Add setup.py skeleton

---
+++ skeletons.dotfile/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='python-package-name',
+    version='0.1',
+    author='Author Name',
+    author_email='author@something.com',
+    description='Short description',
+    packages=[
+        'package_directory',
+        'package_directory.sub_package_directory',
+    ],
+    install_requires=[
+        'dependency==X.Y.Z',
+    ],
+    entry_points={
+        'console_scripts': [
+            'name=path:function',
+        ]
+    }
+)
