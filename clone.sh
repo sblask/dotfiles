84fe6e03405410ffb61315604bf814dd9b51da8b
Sebastian Blask <blask@gmx.de>
2015-12-15 17:04:36 +0100

    Add plugin manager

--- clone.sh
+++ clone.sh
@@ -13,6 +13,10 @@ rm -rf $SCM_BREEZE_CLONE_DIRECTORY
 git clone https://github.com/sblask/scm_breeze.git $SCM_BREEZE_CLONE_DIRECTORY
 patch -p0 --directory=$SCM_BREEZE_CLONE_DIRECTORY < scm_breeze_patch
 
+TPM_CLONE_DIRECTORY=$DOTFILES_DIRECTORY/.tmux/plugins/tpm.copy
+rm -rf $TPM_CLONE_DIRECTORY
+git clone https://github.com/tmux-plugins/tpm $TPM_CLONE_DIRECTORY
+
 FONTS_CLONE_DIRECTORY=$DOTFILES_DIRECTORY/.fonts
 mkdir -p $FONTS_CLONE_DIRECTORY
 wget https://github.com/Lokaltog/powerline-fonts/raw/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf -O $FONTS_CLONE_DIRECTORY/DejaVu_Sans_Mono_for_Powerline.ttf.symlink
