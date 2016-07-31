a63d43a83c6e7c2ca756521bc76b49998f2e337c
Sebastian Blask <blask@gmx.de>
2016-07-31 03:02:18 +0200

    Make devilspie work better

--- devilspie2.dotfile/maximize.lua
+++ devilspie2.dotfile/maximize.lua
@@ -1,24 +1,32 @@
+-- maximus does not handle undecoration on window creation correctly and
+-- maximizes too many things, so use devilspie for undecoration and rely on
+-- windows remembering their maximized state (there are exceptions where this
+-- does not work)
+-- Use `maxmius --no-maximize` so that it handles decoration/undecoration on
+-- unmaximize/maximize
+
+-- Use --debug to get debug printing
 debug_print("")
 debug_print("Application: " .. get_application_name())
 debug_print("Window Name: " .. get_window_name());
 debug_print("Window Type: " .. get_window_type());
+debug_print("Maximized: " .. tostring(get_window_is_maximized()));
+debug_print("Maximized Vertically: " .. tostring(get_window_is_maximized_vertically()));
+debug_print("Maximized Horizontally: " .. tostring(get_window_is_maximized_horizontally()));
 
-if (get_application_name() == "Document Viewer" and get_window_type() == "WINDOW_TYPE_NORMAL") then
-    maximize();
-    undecorate_window();
+-- Handle the above mentioned exceptions
+if (get_window_type() == "WINDOW_TYPE_NORMAL") then
+    if (
+        get_application_name() == "VLC media player" or
+        get_application_name() == "xterm" or
+        false
+    ) then
+        unmaximize(); -- make sure maximize works, sometimes it doesn't
+        maximize();
+        undecorate_window();
+    end
 end
 
-if (get_application_name() == "Firefox" and get_window_type() == "WINDOW_TYPE_NORMAL") then
-    maximize();
-    undecorate_window();
-end
-
-if (get_application_name() == "Meld" and get_window_type() == "WINDOW_TYPE_NORMAL") then
-    maximize();
-    undecorate_window();
-end
-
-if (get_application_name() == "xterm" and get_window_type() == "WINDOW_TYPE_NORMAL") then
-    maximize();
+if (get_window_is_maximized_horizontally() and get_window_is_maximized_vertically()) then
     undecorate_window();
 end
