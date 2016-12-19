7996404f088999a19a330f3923885c665d275b1d
Sebastian Blask <blask@gmx.de>
2016-12-19 15:38:32 +0100

    Add git flow completions

--- clone.sh
+++ clone.sh
@@ -43,6 +43,7 @@ wget https://raw.githubusercontent.com/zsh-users/zsh-completions/master/src/_sca
 wget https://raw.githubusercontent.com/zsh-users/zsh-completions/master/src/_setup.py
 wget https://raw.githubusercontent.com/zsh-users/zsh-completions/master/src/_vagrant
 wget https://raw.githubusercontent.com/zsh-users/zsh-completions/master/src/_virtualbox
+wget https://raw.githubusercontent.com/petervanderdoes/git-flow-completion/develop/git-flow-completion.zsh -O _git-flow && echo "#compdef git-flow" | cat - _git-flow | sponge _git-flow
 popd
 
 wget https://github.com/github/hub/releases/download/v2.2.0-preview1/hub_2.2.0-preview1_linux_amd64.gz.tar -O $DOTFILES_DIRECTORY/hub.tgz
