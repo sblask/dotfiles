7b5fdb336f5fe2316e625f04bfb15255db5a49f4
Sebastian Blask <blask@gmx.de>
2017-01-31 03:00:14 +0100

    Replace scm breeze aliases with functions for compatibility with zsh completions

--- zshrc.d.dotfile/scm_breeze.zsh
+++ zshrc.d.dotfile/scm_breeze.zsh
@@ -1,3 +1,24 @@
 if [ -f ~/.scm_breeze/scm_breeze.sh ]; then
     . ~/.scm_breeze/scm_breeze.sh
 fi
+
+# the aliases from scm breeze don't play nicely with zsh completions
+# so use functions instead
+
+for builtin in \
+    cd \
+; do
+    eval "function ${builtin} { exec_scmb_expand_args builtin ${builtin} \$@ }"
+done
+
+for command in \
+    cat \
+    cp \
+    ln \
+    mv \
+    rm \
+    vim \
+    vimdiff \
+; do
+    eval "function ${command} { exec_scmb_expand_args $(which ${command}) \$@ }"
+done
