02251ad0bd818a5f743ff25842c986092b4b1cf9
Sebastian Blask <blask@gmx.de>
2017-02-16 17:27:40 +0100

    Autoenable virtualenvs in any parent or current folder

--- zshrc.d.dotfile/auto_enable_virtualenv.zsh
+++ zshrc.d.dotfile/auto_enable_virtualenv.zsh
@@ -1,21 +1,20 @@
 function maybe-install-virtualenv-requirements {
-    VIRTUALENV_REQUIREMENTS=$HOME/.virtualenvs/requirements.txt
-    if ! python -c "import pkg_resources; pkg_resources.require(open('$VIRTUALENV_REQUIREMENTS').read().splitlines())" 2>/dev/null; then
+    VIRTUALENV_REQUIREMENTS=${HOME}/.virtualenvs/requirements.txt
+    if ! python -c "import pkg_resources; pkg_resources.require(open('${VIRTUALENV_REQUIREMENTS}').read().splitlines())" 2>/dev/null; then
         echo -e "$fg[yellow]Installing requirements from ${VIRTUALENV_REQUIREMENTS}${reset_color}"
-        pip install --upgrade --requirement $VIRTUALENV_REQUIREMENTS
+        pip install --upgrade --requirement ${VIRTUALENV_REQUIREMENTS}
     fi
 }
 
 function auto-enable-virtualenv {
-    if [ -d .venv ]; then
-        if [ "$VIRTUAL_ENV" = "" ]; then
-            source .venv/bin/activate
-            maybe-install-virtualenv-requirements
-        fi
+    VIRTUALENV_DIRECTORY=$(print -l (../)#.venv(N:a))
+    if [ "${VIRTUALENV_DIRECTORY}" != "" -a -d ${VIRTUALENV_DIRECTORY} ]; then
+        source ${VIRTUALENV_DIRECTORY}/bin/activate
+        maybe-install-virtualenv-requirements
     elif [ -f .penv -o -f .venv ]; then
         CURRENT_DIRECTORY=${PWD##*/}
-        if [ "($CURRENT_DIRECTORY) " != "$(virtual_env)" ]; then
-            workon $CURRENT_DIRECTORY
+        if [ "(${CURRENT_DIRECTORY}) " != "$(virtual_env)" ]; then
+            workon ${CURRENT_DIRECTORY}
         fi
     fi
 }
