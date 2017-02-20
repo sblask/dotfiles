c0c892f5016baf7fcd5737fa5a80232ced5a332a
Sebastian Blask <blask@gmx.de>
2017-02-20 15:28:19 +0100

    Add gdh function

--- zshrc.d.dotfile/functions.zsh
+++ zshrc.d.dotfile/functions.zsh
@@ -4,6 +4,9 @@ function gd {
 function gdc {
     git diff --color=always $@ --cached | vimpager
 }
+function gdh {
+    git diff --color=always $@          | head --lines 50
+}
 function mcd {
     mkdir $1 && cd $1
 }
