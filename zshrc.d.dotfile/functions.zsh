57ee373d5eea9b45c2c670dccd21baa25d3cb938
Sebastian Blask <blask@gmx.de>
2017-03-30 11:27:44 +0200

    Configure vimpager for git globally instead of aliases only

--- zshrc.d.dotfile/functions.zsh
+++ zshrc.d.dotfile/functions.zsh
@@ -8,18 +8,21 @@ function expand-scm-breeze-index-or-expand-or-complete {
         zle expand-or-complete
     fi
 }
+
 function gd {
-    git diff --color=always $@          | vimpager
+    git diff --color=always $@
 }
 function gdc {
-    git diff --color=always $@ --cached | vimpager
+    git diff --color=always $@ --cached
 }
 function gdh {
     git diff --color=always $@          | head --lines 50
 }
+
 function mcd {
     mkdir $1 && cd $1
 }
+
 function ts {
     date       --iso-8601=seconds --date @$1 2>/dev/null || date --date $1 +%s 2>/dev/null
 }
