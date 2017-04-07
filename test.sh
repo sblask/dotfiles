cd1afcf931a2be33ea78e248eb827eccc1a8f05c
Sebastian Blask <blask@gmx.de>
2017-04-07 22:28:40 +0200

    Test things with Travis

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@ set -o errexit -o nounset -o pipefail -o xtrace
 
 SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 
-diff <(add-index ls_list_in) <(cat ls_list_out)
-diff <(add-index --print-files ls_list_plus_files_in) <(cat ls_list_plus_files_out)
+for file in $SCRIPT_DIRECTORY/test/*.sh
+do
+    . $file
+done
+
 echo "SUCCESS"
