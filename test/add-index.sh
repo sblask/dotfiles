52513023db2ee6c226b505524e778ed1f17bc6eb
Sebastian Blask <blask@gmx.de>
2017-04-07 22:31:28 +0200

    Fix build

--- test/add-index.sh
+++ test/add-index.sh
@@ -5,11 +5,11 @@ set -o errexit -o nounset -o pipefail -o xtrace
 SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 
 set +o xtrace
-actual=$( add-index $SCRIPT_DIRECTORY/ls_list_in )
+actual=$( $SCRIPT_DIRECTORY/../.bin/add-index.symlink $SCRIPT_DIRECTORY/ls_list_in )
 set -o xtrace
 colordiff --unified <( set +o xtrace; echo "$actual" ) $SCRIPT_DIRECTORY/ls_list_out
 
 set +o xtrace
-actual=$( add-index --print-files $SCRIPT_DIRECTORY/ls_list_plus_files_in )
+actual=$( $SCRIPT_DIRECTORY/../.bin/add-index.symlink --print-files $SCRIPT_DIRECTORY/ls_list_plus_files_in )
 set -o xtrace
 colordiff --unified <( set +o xtrace; echo "$actual" ) $SCRIPT_DIRECTORY/ls_list_plus_files_out
