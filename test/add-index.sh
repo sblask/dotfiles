b4d1b99b8f6895ff0e4b67f28ed5fdf11212a4e4
Sebastian Blask <blask@gmx.de>
2017-04-09 23:22:49 +0200

    Add input type argument

--- test/add-index.sh
+++ test/add-index.sh
@@ -5,16 +5,16 @@ set -o errexit -o nounset -o pipefail -o xtrace
 SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 
 set +o xtrace
-actual=$( $SCRIPT_DIRECTORY/../.bin/add-index.symlink $SCRIPT_DIRECTORY/ls_list_in )
+actual=$( $SCRIPT_DIRECTORY/../.bin/add-index.symlink --input-type ls_list $SCRIPT_DIRECTORY/ls_list_in )
 set -o xtrace
 colordiff --unified <( set +o xtrace; echo "$actual" ) $SCRIPT_DIRECTORY/ls_list_out
 
 set +o xtrace
-actual=$( $SCRIPT_DIRECTORY/../.bin/add-index.symlink --print-files $SCRIPT_DIRECTORY/ls_list_in )
+actual=$( $SCRIPT_DIRECTORY/../.bin/add-index.symlink --input-type ls_list --print-files $SCRIPT_DIRECTORY/ls_list_in )
 set -o xtrace
 colordiff --unified <( set +o xtrace; echo "$actual" ) $SCRIPT_DIRECTORY/ls_list_plus_files_out
 
 set +o xtrace
-actual=$( $SCRIPT_DIRECTORY/../.bin/add-index.symlink --print-files $SCRIPT_DIRECTORY/ls_list_colour_in )
+actual=$( $SCRIPT_DIRECTORY/../.bin/add-index.symlink --input-type ls_list --print-files $SCRIPT_DIRECTORY/ls_list_colour_in )
 set -o xtrace
 colordiff --unified <( set +o xtrace; echo "$actual" ) $SCRIPT_DIRECTORY/ls_list_colour_plus_files_out
