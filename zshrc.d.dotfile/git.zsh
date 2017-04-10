cb497a82554ec7f80f2ded683a045964a6afb3a3
Sebastian Blask <blask@gmx.de>
2017-04-10 10:45:15 +0200

    Add some git aliases

--- zshrc.d.dotfile/git.zsh
+++ zshrc.d.dotfile/git.zsh
@@ -5,8 +5,10 @@ alias gash='git stash --include-untracked --keep-index'
 alias gashl='git stash list'
 alias gashp='git stash pop'
 alias gc='git commit'
+alias gco='git checkout'
 alias gdt='git difftool'
 alias gdtc='gdt --cached'
+alias gf='git fetch'
 alias glog='git log --graph --decorate --pretty=format:$GIT_LOG_FORMAT --abbrev-commit --date=short --abbrev=8'
 alias gl='glog'
 alias glh='glog | head'
@@ -17,6 +19,7 @@ alias gldo='glog --left-right --graph'
 alias gmd='git merge --ff-only origin/develop'
 alias gmm='git merge --ff-only origin/master'
 alias gps='git push'
+alias grp='git rebase'
 
 function gd {
     git diff --color=always          $@
