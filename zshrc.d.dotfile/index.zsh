227136594e246f492c1a86843f92999e77b94976
Sebastian Blask <blask@gmx.de>
2017-04-12 00:22:02 +0200

    Enable completions for alias functions

--- zshrc.d.dotfile/index.zsh
+++ zshrc.d.dotfile/index.zsh
@@ -77,15 +77,19 @@ function __print-path-argument {
 function gb {
     git branch                                         $@   | add-index --input-type git_branch --print-indexables | set-index-variables
 }
+compdef _git-branch gb
 
 function gs {
     git status --untracked-files=all                   $@   | add-index --input-type git_status --print-indexables | set-index-variables
 }
+compdef _git-status gs
 
 function la {
     { __print-path-argument $@; ls -lha --color=always $@ } | add-index --input-type ls_list    --print-indexables | set-index-variables
 }
+compdef _ls la
 
 function ll {
     { __print-path-argument $@; ls -lh  --color=always $@ } | add-index --input-type ls_list    --print-indexables | set-index-variables
 }
+compdef _ls ll
