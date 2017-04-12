1f7a6b3a14f89196374a7ecc6b4b090159b4bd33
Sebastian Blask <blask@gmx.de>
2017-04-12 12:40:53 +0200

    Cleanup git aliases

--- zshrc.d.dotfile/git.zsh
+++ zshrc.d.dotfile/git.zsh
@@ -1,13 +1,15 @@
 GIT_LOG_FORMAT='%C(blue)%h%Creset -%C(yellow)%d%Creset %s %C(blue)(%ai) %C(bold blue)<%an (%ae)>%Creset'
 alias ga='git add'
-alias gap='ga --patch'
+alias gap='git add --patch'
 alias gash='git stash --include-untracked --keep-index'
 alias gashl='git stash list'
 alias gashp='git stash pop'
 alias gc='git commit'
 alias gco='git checkout'
+alias gd='git diff --color=always'
+alias gdc='git diff --color=always --cached'
 alias gdt='git difftool'
-alias gdtc='gdt --cached'
+alias gdtc='git difftool --cached'
 alias gf='git fetch'
 alias glog='git log --graph --decorate --pretty=format:$GIT_LOG_FORMAT --abbrev-commit --date=short --abbrev=8'
 alias gl='glog'
@@ -21,14 +23,3 @@ alias gmd='git merge --ff-only origin/develop'
 alias gmm='git merge --ff-only origin/master'
 alias gps='git push'
 alias grb='git rebase'
-
-function gd {
-    git diff --color=always          $@
-}
-function gdc {
-    git diff --color=always --cached $@
-}
-function gdh {
-    git diff --color=always          $@ | head --lines 50
-}
-
