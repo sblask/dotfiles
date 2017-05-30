30b864d895f9fb7277544f7bcc119318ed6fad14
Sebastian Blask <blask@gmx.de>
2017-05-31 01:01:37 +0200

    Add alias to generate extension icons

--- zshrc.d.dotfile/aliases_misc.zsh
+++ zshrc.d.dotfile/aliases_misc.zsh
@@ -1,11 +1,12 @@
 alias cal='echo -n "       Week "; week;ncal -wC'
 alias diff='colordiff'
+alias extensionIcons='for size in 32x32 64x64 128x128 440x280; do convert -background none -density 1000 -resize ${size} -extent ${size} -gravity center icon.svg icon-${size}.png; done'
+alias extensionUpdateVersion='version=$(jq --raw-output .version manifest.json) && vared -p "Current version is ${version}, specify new version: " version && jq --indent 4 ".version = \"$version\"" manifest.json | sponge manifest.json && jq --indent 4 ".version = \"$version\"" package.json | sponge package.json'
 alias less='less -K'
 alias psgrep='ps aufx | grep'
 alias psless='ps aufx | vimpager'
 alias pss='ps aufx'
 alias untar='tar -xzf'
-alias updateVersion='version=$(jq --raw-output .version manifest.json) && vared -p "Current version is ${version}, specify new version: " version && jq --indent 4 ".version = \"$version\"" manifest.json | sponge manifest.json && jq --indent 4 ".version = \"$version\"" package.json | sponge package.json'
 alias week='date +%W'
 # allow vim instead of less
 alias vimpager="vim -c 'set tabstop=4 nomodified nomodifiable nolist nowrap' -c 'AnsiEsc' -"
