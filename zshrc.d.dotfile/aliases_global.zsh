7075cc7f6d082aff8fd1c5b7fcbe5818875ffe53
Sebastian Blask <blask@gmx.de>
2017-09-25 09:05:09 +0200

    Improve index aliases

--- zshrc.d.dotfile/aliases_global.zsh
+++ zshrc.d.dotfile/aliases_global.zsh
@@ -1,4 +1,5 @@
 alias -g C="| wc --lines"
 alias -g H="| head --lines 25"
-alias -g I="| add-index --input-type mixed --print-indexables | set-index-variables"
+alias -g I="|  add-index --input-type list  --print-indexables | set-index-variables"
+alias -g IF="| add-index --input-type mixed --print-indexables | set-index-variables"
 alias -g T="| tail --lines 25"
