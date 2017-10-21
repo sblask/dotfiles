cf6b949b46aed75337decde02e0b47a29f0c539f
Sebastian Blask <blask@gmx.de>
2017-10-22 01:25:26 +0200

    Add idempotence check

--- test/ansible.sh
+++ test/ansible.sh
@@ -4,5 +4,10 @@ set -o errexit -o nounset -o pipefail -o xtrace
 
 SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 
-
 ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${SCRIPT_DIRECTORY}/../ansible-test.yml
+
+ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${SCRIPT_DIRECTORY}/../ansible-test.yml \
+  |   grep --quiet 'changed=0.*failed=0' \
+  && (echo 'Idempotence test: pass' && exit 0) \
+  || (echo 'Idempotence test: fail' && exit 1) \
+
