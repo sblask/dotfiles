d25afb993134b0482c19421ee535c089069bc599
Sebastian Blask <blask@gmx.de>
2017-10-22 01:18:54 +0200

    Use temporary directory for tests

--- test/ansible.sh
+++ test/ansible.sh
@@ -5,4 +5,4 @@ set -o errexit -o nounset -o pipefail -o xtrace
 SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 
 
-ansible-playbook ${SCRIPT_DIRECTORY}/../ansible-test.yml
+ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${SCRIPT_DIRECTORY}/../ansible-test.yml
