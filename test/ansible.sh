c3862c62c43a1b066626ef5450299a75026666b5
Sebastian Blask <blask@gmx.de>
2017-10-26 08:40:30 +0200

    Move ansible playbooks into own directory

--- test/ansible.sh
+++ test/ansible.sh
@@ -4,9 +4,11 @@ set -o errexit -o nounset -o pipefail -o xtrace
 
 SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 
-ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${SCRIPT_DIRECTORY}/../ansible-test.yml
+PLAYBOOK=${SCRIPT_DIRECTORY}/../ansible/ansible-test.yml
 
-ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${SCRIPT_DIRECTORY}/../ansible-test.yml \
+ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${PLAYBOOK}
+
+ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${PLAYBOOK} \
   |   grep --quiet 'changed=0.*failed=0' \
   && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1) \
