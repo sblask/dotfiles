90df459824cdebabd6ead8987ea3d1374d677881
Sebastian Blask <blask@gmx.de>
2017-11-06 11:33:32 +0100

    Add global alias to print last column

--- .zshrc.d.symlink/aliases_global.zsh
+++ .zshrc.d.symlink/aliases_global.zsh
@@ -2,4 +2,5 @@ alias -g C="| wc --lines"
 alias -g H="| head --lines 25"
 alias -g I="|  add-index --input-type list  --print-indexables | set-index-variables"
 alias -g IF="| add-index --input-type mixed --print-indexables | set-index-variables"
+alias -g LC="| awk '{print \$NF}'" # print last column
 alias -g T="| tail --lines 25"
