229f43587777e959f2c635da80dfd0df07a77809
Sebastian Blask <blask@gmx.de>
2017-11-22 08:48:24 +0100

    Add declare to shell skeleton

--- .skeletons.symlink/skeleton.sh
+++ .skeletons.symlink/skeleton.sh
@@ -2,4 +2,4 @@
 
 set -o errexit -o nounset -o pipefail -o xtrace
 
-SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
+declare -r SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
