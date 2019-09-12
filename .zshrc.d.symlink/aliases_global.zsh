765ac21bf82d9f5446e1d78deb00cfd4b179a818
Sebastian Blask <blask@gmx.de>
2019-09-12 10:33:43 +0200

    Add global alias for grep -v

--- .zshrc.d.symlink/aliases_global.zsh
+++ .zshrc.d.symlink/aliases_global.zsh
@@ -3,6 +3,7 @@ alias -g  H="| head --lines 25"
 alias -g  I="| add-index --input-type list  --print-indexables | set-index-variables"
 alias -g  T="| tail --lines 25"
 alias -g BC="| awk --field-separator : '{print \$1}'" # print whatever is before the first colon
+alias -g EX="| grep -v "
 alias -g IF="| add-index --input-type mixed --print-indexables | set-index-variables"
 alias -g IT="| add-index --input-type tree  --print-indexables | set-index-variables"
 alias -g LC="| awk '{print \$NF}'" # print last column
