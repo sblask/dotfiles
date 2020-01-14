5da55b9f369f70683cd9fefd2192adb954fe3167
Sebastian Blask <blask@gmx.de>
2020-01-14 15:42:41 +1300

    Remove unsupported command

--- dotfiles_shared/.zshrc.d.symlink/settings_misc.zsh
+++ dotfiles_shared/.zshrc.d.symlink/settings_misc.zsh
@@ -6,8 +6,6 @@ autoload -Uz zmv
 export KEYTIMEOUT=1
 # enable using comments in command line
 setopt INTERACTIVE_COMMENTS
-# propose action if command was not found
-source /etc/zsh_command_not_found
 # automatically push directories to the stack, show stack with dirs -v, change to one using ~index
 setopt AUTO_PUSHD
 # disable Ctrl-S and Ctrl-Q
