269d553fb60593889ed85427b7c05a8cfd2c244b
Sebastian Blask <blask@gmx.de>
2020-01-14 15:37:52 +1300

    Add support for changing cursor shape for apple terminal

--- dotfiles_shared/.zshrc.d.symlink/cursor.zsh
+++ dotfiles_shared/.zshrc.d.symlink/cursor.zsh
@@ -6,6 +6,12 @@ function zle-line-init zle-keymap-select {
         else
             echo -e -n "\x1b[\x35 q" # changes to blinking bar
         fi
+    elif [ "$TERM_PROGRAM" = "Apple_Terminal" ]; then
+        if [ "${KEYMAP}" = "vicmd" ]; then
+            echo -e -n "\033[1 q" # changes to blinking block
+        else
+            echo -e -n "\033[5 q" # changes to blinking bar
+        fi
     fi
 }
 zle -N zle-line-init
