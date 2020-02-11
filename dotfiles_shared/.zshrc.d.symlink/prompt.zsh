b445578c718efab335296a9ba9c0748706274103
Sebastian Blask <blask@gmx.de>
2020-02-11 17:47:52 +1300

    Make prompt helper functions private

--- dotfiles_shared/.zshrc.d.symlink/prompt.zsh
+++ dotfiles_shared/.zshrc.d.symlink/prompt.zsh
@@ -1,38 +1,38 @@
-function aws_region {
+function __aws_region {
     if [ "$AWS_DEFAULT_REGION" = "" ]; then
         echo ""
     else
         echo "$AWS_DEFAULT_REGION "
     fi
 }
-function conda_env {
+function __conda_env {
     if [ "$CONDA_DEFAULT_ENV" = "" ]; then
         echo ""
     else
         echo "(conda: $(print -rD $CONDA_DEFAULT_ENV)) "
     fi
 }
-function git_no_mail_warning {
+function __git_no_mail_warning {
     git status 1>/dev/null 2>/dev/null
     if [ $? -ne 128 -a $? -ne 127 -a "$(git config user.email)" = "" -a "$EMAIL" = "" ]; then
         echo -n '(user.email not configured) '
     fi
 }
-function my_ip {
+function __my_ip {
     if [ $OS = mac ]; then
         ipconfig getifaddr en0
     else
         hostname --all-ip-addresses | awk '{print $1}'
     fi
 }
-function imsa_profile {
+function __imsa_profile {
     if [ "$IMSA_PROFILE" = "" ]; then
         echo ""
     else
         echo "($IMSA_PROFILE until $(date --date $IMSA_PROFILE_EXPIRATION +%H:%M)) "
     fi
 }
-function virtual_env {
+function __virtual_env {
     if [ "$VIRTUAL_ENV" = "" ]; then
         echo ""
     else
@@ -41,21 +41,21 @@ function virtual_env {
 }
 
 
-function print-first-line {
+function __print_first_line {
     print
     lines=(
         "$(date +'[%a, %e %b %Y, %H:%M:%S]') "
-        "$fg[yellow]$(aws_region)$reset_color"
-        "$fg[yellow]$(imsa_profile)$reset_color"
-        "$fg[blue]$(virtual_env)$(conda_env)$reset_color"
+        "$fg[yellow]$(__aws_region)$reset_color"
+        "$fg[yellow]$(__imsa_profile)$reset_color"
+        "$fg[blue]$(__virtual_env)$(__conda_env)$reset_color"
         "%n " # username
         "$fg[blue]%m$reset_color " # hostname
-        "$(my_ip) "
+        "$(__my_ip) "
         "$(git_super_status) "
-        "$fg[red]$(git_no_mail_warning)$reset_color"
+        "$fg[red]$(__git_no_mail_warning)$reset_color"
     )
     print -rP $(IFS=; echo "${lines[*]}")
 }
 
 PS1="%~ $ "
-add-zsh-hook precmd print-first-line
+add-zsh-hook precmd __print_first_line
