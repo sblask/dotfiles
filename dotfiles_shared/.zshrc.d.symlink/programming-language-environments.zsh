ac12d1bbe68453f6b3ad67d3fe7b8c0e2c7c50bf
Sebastian Blask <blask@gmx.de>
2020-02-14 11:12:40 +1300

    Support ruby gem environment

--- dotfiles_shared/.zshrc.d.symlink/programming-language-environments.zsh
+++ dotfiles_shared/.zshrc.d.symlink/programming-language-environments.zsh
@@ -11,3 +11,8 @@ fi
 if [ -f "$HOME/.rvm/scripts/rvm" ]; then
     . "$HOME/.rvm/scripts/rvm"
 fi
+
+# Ruby gem environment
+if [ -d "$HOME/.gem/ruby/2.6.0/bin" ]; then
+    PATH="$HOME/.gem/ruby/2.6.0/bin:$PATH"
+fi
