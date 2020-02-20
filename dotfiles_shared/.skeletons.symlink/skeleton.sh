31b7432d57be326770e4e9e3fb0351a81ea53cb9
Sebastian Blask <blask@gmx.de>
2020-02-20 13:09:58 +1300

    Add for and if/else to shell skeleton

--- dotfiles_shared/.skeletons.symlink/skeleton.sh
+++ dotfiles_shared/.skeletons.symlink/skeleton.sh
@@ -3,3 +3,16 @@
 set -o errexit -o nounset -o pipefail -o xtrace
 
 declare -r SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
+
+if [ "$1" = "something" ]; then
+    echo "if"
+elif [ "$1" = "something else" ]; then
+    echo "elif"
+else
+    echo "else"
+fi
+
+for argument in $( echo $@ )
+do
+     echo "for"
+done
