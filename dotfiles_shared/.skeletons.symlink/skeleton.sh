248e7ad45877048704935e25a8f293d2acd17a28
Sebastian Blask <blask@gmx.de>
2020-02-27 12:46:30 +1300

    Add function to shell skeleton

--- dotfiles_shared/.skeletons.symlink/skeleton.sh
+++ dotfiles_shared/.skeletons.symlink/skeleton.sh
@@ -4,15 +4,17 @@ set -o errexit -o nounset -o pipefail -o xtrace
 
 declare -r SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 
-if [ "$1" = "something" ]; then
-    echo "if"
-elif [ "$1" = "something else" ]; then
-    echo "elif"
-else
-    echo "else"
-fi
+function do-something {
+    if [ "$1" = "something" ]; then
+        echo "if"
+    elif [ "$1" = "something else" ]; then
+        echo "elif"
+    else
+        echo "else"
+    fi
 
-for argument in $( echo $@ )
-do
-     echo "for"
-done
+    for argument in $( echo $@ )
+    do
+         echo "for"
+    done
+}
