881d7b3b697e08adb5c11a400e8e0f72979e4730
Sebastian Blask <blask@gmx.de>
2020-06-05 12:43:27 +1200

    Add verbose flags to ansible

--- test_files/ansible.sh
+++ test_files/ansible.sh
@@ -6,8 +6,8 @@ SCRIPT_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE:-$0}" )" && pwd )
 
 PLAYBOOK=${SCRIPT_DIRECTORY}/../ansible/ansible-test.yml
 
-ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${PLAYBOOK}
-ansible-playbook --extra-vars target_directory=/tmp/dotfiles-ansible-test ${PLAYBOOK} > ansible_result
+ansible-playbook --verbose --verbose --verbose --extra-vars target_directory=/tmp/dotfiles-ansible-test ${PLAYBOOK}
+ansible-playbook --verbose --verbose --verbose --extra-vars target_directory=/tmp/dotfiles-ansible-test ${PLAYBOOK} > ansible_result
 
 cat ansible_result | grep --quiet 'changed=0.*failed=0' \
   && (echo 'Idempotence test: pass' && exit 0) \
