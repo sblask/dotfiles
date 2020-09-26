7f4645e4a951b93b1db74321c729e42314db9128
Sebastian Blask <blask@gmx.de>
2020-09-26 23:30:20 +1200

    Add cursor shape support for ZSH in xfce4-terminal

--- dotfiles_shared/.config/zsh/config.d.symlink/cursor.zsh
+++ dotfiles_shared/.config/zsh/config.d.symlink/cursor.zsh
@@ -1,16 +1,17 @@
 function zle-line-init zle-keymap-select {
     # for cursor shapes, see http://stackoverflow.com/questions/4416909/anyway-change-the-cursor-vertical-line-instead-of-a-box
-    if [ "$XTERM_VERSION" != "" ]; then
-        if [ "${KEYMAP}" = "vicmd" ]; then
-            echo -e -n "\x1b[\x31 q" # changes to blinking block
-        else
-            echo -e -n "\x1b[\x35 q" # changes to blinking bar
-        fi
-    elif [ "$TERM_PROGRAM" = "Apple_Terminal" ]; then
+    if [ "$TERM_PROGRAM" = "Apple_Terminal" ]; then
         if [ "${KEYMAP}" = "vicmd" ]; then
             echo -e -n "\033[1 q" # changes to blinking block
         else
             echo -e -n "\033[5 q" # changes to blinking bar
+        fi
+    # xterm and xfce4-terminal
+    else
+        if [ "${KEYMAP}" = "vicmd" ]; then
+            echo -e -n "\x1b[\x31 q" # changes to blinking block
+        else
+            echo -e -n "\x1b[\x35 q" # changes to blinking bar
         fi
     fi
 }
