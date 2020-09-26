6163be2f8d830c2ab98e05ec63328926f3fbfd50
Sebastian Blask <blask@gmx.de>
2020-09-27 00:20:25 +1200

    Make the necessary config changes after switching to zsh-git-prompt/zsh-git-prompt

--- dotfiles_shared/.config/zsh/config.d.symlink/plugins.zsh
+++ dotfiles_shared/.config/zsh/config.d.symlink/plugins.zsh
@@ -11,46 +11,16 @@ ZSH_HIGHLIGHT_PATTERNS+=('rm -rf*' 'fg=black,bg=red')
 ZSH_HIGHLIGHT_PATTERNS+=('\|' 'fg=yellow')
 ZSH_HIGHLIGHT_PATTERNS+=('\*' 'fg=black,bg=red')
 
-# configure git prompt plugin
-function git_super_status {
-    precmd_update_git_vars
-    if [ "$__CURRENT_GIT_STATUS" != "" ] && [ "$__CURRENT_GIT_STATUS" != ": 0 0 0 0 0 0" ]; then
-        STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
-        if [ "$GIT_BEHIND" -ne "0" ] && [ "$GIT_AHEAD" -ne "0" ]; then
-            STATUS="$STATUS $ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND/$ZSH_THEME_GIT_PROMPT_AHEAD$GIT_AHEAD%{${reset_color}%}"
-        elif [ "$GIT_BEHIND" -ne "0" ]; then
-            STATUS="$STATUS $ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND%{${reset_color}%}"
-        elif [ "$GIT_AHEAD" -ne "0" ]; then
-            STATUS="$STATUS $ZSH_THEME_GIT_PROMPT_AHEAD$GIT_AHEAD%{${reset_color}%}"
-        fi
-        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
-        if [ "$GIT_STAGED" -ne "0" ]; then
-            STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
-        fi
-        if [ "$GIT_CONFLICTS" -ne "0" ]; then
-            STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
-        fi
-        if [ "$GIT_CHANGED" -ne "0" ]; then
-            STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
-        fi
-        if [ "$GIT_UNTRACKED" -ne "0" ]; then
-            STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED%{${reset_color}%}"
-        fi
-        if [ "$GIT_CHANGED" -eq "0" ] && [ "$GIT_CONFLICTS" -eq "0" ] && [ "$GIT_STAGED" -eq "0" ] && [ "$GIT_UNTRACKED" -eq "0" ]; then
-            STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CLEAN"
-        fi
-        STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
-        echo "$STATUS"
-    fi
-}
 ZSH_THEME_GIT_PROMPT_PREFIX="("
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"
+ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[blue]%}"
 ZSH_THEME_GIT_PROMPT_SEPARATOR=""
 ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg[red]-%G%}"
+ZSH_THEME_GIT_PROMPT_BEHIND_AHEAD_SEPARATOR="$fg[red]/"
 ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[red]+%G%}"
-ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[blue]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{ ✔%G%}"
 ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[yellow]%}%{ +%G%}"
 ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[green]%}%{ ~%G%}"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%}%{ …%G%}"
+ZSH_THEME_GIT_PROMPT_STASHED="%{$fg_bold[magenta]%}%{ ⚑%G%}"
 ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{X%G%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX=")"
