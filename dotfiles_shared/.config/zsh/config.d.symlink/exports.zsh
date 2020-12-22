211dde0f2f4b3f6a6b6cdc760498279ea46bce24
Sebastian Blask <blask@gmx.de>
2020-12-22 20:56:03 +1300

    Remove AWS_DEFAULT_REGION - allow aws-vault to take care of it

--- dotfiles_shared/.config/zsh/config.d.symlink/exports.zsh
+++ dotfiles_shared/.config/zsh/config.d.symlink/exports.zsh
@@ -1,4 +1,3 @@
-export AWS_DEFAULT_REGION=ap-southeast-2
 export EDITOR=vim
 export LS_COLORS='di=34:ln=33:or=31:ex=32'
 export MANPAGER="/bin/sh -c \"col -b | vim -c 'set filetype=man tabstop=8 nomodified nomodifiable nolist nospell nonumber' -\""
