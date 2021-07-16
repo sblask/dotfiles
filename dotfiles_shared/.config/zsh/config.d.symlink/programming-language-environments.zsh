2f91e05f47352b3c5feb24095679f2b47f9830b5
Sebastian Blask <blask@gmx.de>
2021-07-16 20:45:24 +1200

    Move things around to get a clean PATH

--- dotfiles_shared/.config/zsh/config.d.symlink/programming-language-environments.zsh
+++ dotfiles_shared/.config/zsh/config.d.symlink/programming-language-environments.zsh
@@ -1,23 +1,4 @@
-# Node version manager
-export N_PREFIX="$HOME/.n"
-if [ -d "$N_PREFIX/bin" ]; then
-    PATH="$N_PREFIX/bin:$PATH"
-fi
-
 # Python version manager
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
-fi
-
-# Ruby version manager
-if [ -d "$HOME/.rvm/bin" ]; then
-    PATH="$HOME/.rvm/bin:$PATH"
-fi
-if [ -f "$HOME/.rvm/scripts/rvm" ]; then
-    . "$HOME/.rvm/scripts/rvm"
-fi
-
-# Ruby gem environment
-if [ -d "$HOME/.gem/ruby/2.6.0/bin" ]; then
-    PATH="$HOME/.gem/ruby/2.6.0/bin:$PATH"
 fi
