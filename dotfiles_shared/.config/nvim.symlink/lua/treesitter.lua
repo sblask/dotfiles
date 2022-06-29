7018d647ef708d6f5dcfc18e8eb6020ba9ed6b60
Sebastian Blask <blask@gmx.de>
2022-06-29 14:34:26 +1200

    Disable phpdoc
    See https://github.com/claytonrcarter/tree-sitter-phpdoc/issues/15

--- dotfiles_shared/.config/nvim.symlink/lua/treesitter.lua
+++ dotfiles_shared/.config/nvim.symlink/lua/treesitter.lua
@@ -6,7 +6,7 @@ require'nvim-treesitter.configs'.setup {
   sync_install = false,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = {},
+  ignore_install = {"phpdoc"},
 
   highlight = {
     -- `false` will disable the whole extension
