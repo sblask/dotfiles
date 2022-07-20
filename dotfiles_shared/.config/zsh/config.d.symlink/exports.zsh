0bf39e1a4f87343ae54b7c664bef85a7abb06356
Sebastian Blask <blask@gmx.de>
2022-07-20 13:01:55 +1200

    Add ripgrep config

--- dotfiles_shared/.config/zsh/config.d.symlink/exports.zsh
+++ dotfiles_shared/.config/zsh/config.d.symlink/exports.zsh
@@ -3,4 +3,5 @@ export LS_COLORS='di=34:ln=33:or=31:ex=32'
 export MANPAGER="/bin/sh -c \"col -b | nvim -c 'set filetype=man tabstop=8 nomodified nomodifiable nolist nospell nonumber' -\""
 export PAGER='less -K'
 export PIP_DOWNLOAD_CACHE=~/.pip_download_cache
+export RIPGREP_CONFIG_PATH=~/.ripgreprc
 export VIRTUAL_ENV_DISABLE_PROMPT=True
