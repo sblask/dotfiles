4d068c66ddeee439c0a087494ffbe8f0c4be0b39
Sebastian Blask <blask@gmx.de>
2023-09-13 14:40:06 +1200

    Fix MANPAGER command for AppImage nvim

--- dotfiles_shared/.config/zsh/config.d.symlink/exports.zsh
+++ dotfiles_shared/.config/zsh/config.d.symlink/exports.zsh
@@ -1,7 +1,7 @@
 export AWS_CLI_AUTO_PROMPT=on-partial
 export EDITOR=nvim
 export LS_COLORS='di=34:ln=33:or=31:ex=32'
-export MANPAGER="/bin/sh -c \"col -b | nvim -c 'set filetype=man tabstop=8 nomodified nomodifiable nolist nospell nonumber' -\""
+export MANPAGER="/bin/sh -c \"col -b | nvim --appimage-extract-and-run -c 'set filetype=man tabstop=8 nomodified nomodifiable nolist nospell nonumber' -\""
 export PAGER='less -K'
 export PIP_DOWNLOAD_CACHE=~/.pip_download_cache
 export RIPGREP_CONFIG_PATH=~/.ripgreprc
