263b034127ab632a6e1a7f36493809c15c6d30c8
Sebastian Blask <blask@gmx.de>
2024-03-21 21:02:39 +1300

    Add typescript's go to source definition

--- dotfiles_shared/.config/nvim.symlink/lua/lsp.lua
+++ dotfiles_shared/.config/nvim.symlink/lua/lsp.lua
@@ -113,7 +113,28 @@ lspconfig["pylsp"].setup({
 lspconfig["tsserver"].setup({
     on_attach = function(_client, buffer)
         setup_format_on_save(buffer)
+
+        local function goto_source_definition()
+            local position_params = vim.lsp.util.make_position_params()
+            vim.lsp.buf.execute_command({
+                command = "_typescript.goToSourceDefinition",
+                arguments = { vim.api.nvim_buf_get_name(0), position_params.position },
+            })
+        end
+        local opts = { buffer = buffer }
+        vim.keymap.set("n", "<leader>ds", goto_source_definition, opts)
     end,
+    handlers = {
+        ["workspace/executeCommand"] = function(_err, result, ctx, _config)
+            if ctx.params.command ~= "_typescript.goToSourceDefinition" then
+                return
+            end
+            if result == nil or #result == 0 then
+                return
+            end
+            vim.lsp.util.jump_to_location(result[1], "utf-8")
+        end,
+    },
 })
 
 lspconfig["terraformls"].setup({
