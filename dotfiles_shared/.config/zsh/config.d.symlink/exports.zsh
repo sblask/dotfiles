5be6e0fd3731be15ee2ff14e732db16d81187f4b
Sebastian Blask <blask@gmx.de>
2024-07-03 12:25:43 +1200

    Add poetry configuration variable

--- dotfiles_shared/.config/zsh/config.d.symlink/exports.zsh
+++ dotfiles_shared/.config/zsh/config.d.symlink/exports.zsh
@@ -4,6 +4,7 @@ export LS_COLORS='di=34:ln=33:or=31:ex=32'
 export PAGER='less -K'
 export PIP_DOWNLOAD_CACHE=~/.pip_download_cache
 export PIP_REQUIRE_VIRTUALENV=true
+export POETRY_VIRTUALENVS_IN_PROJECT=true
 export RIPGREP_CONFIG_PATH=~/.ripgreprc
 export VIRTUAL_ENV_DISABLE_PROMPT=True
 
