39fa6d718ec17b24b319eba493642832abd67a3e
Sebastian Blask <blask@gmx.de>
2026-05-26 18:50:54 +1200

    Rewrite vim init in Lua

---
+++ dotfiles_shared/.config/nvim.symlink/init.lua
@@ -0,0 +1,38 @@
+local python = vim.fn.expand("~/.opt/pynvim/bin/python")
+
+if vim.fn.executable(python) == 1 then
+    vim.g.python3_host_prog = python
+end
+
+local config_dir = "~/.config/nvim/"
+
+-- Load plugins first as other configs require parts of them
+vim.cmd.source(config_dir .. "vim/plugins.vim")
+
+local vim_files = vim.fn.glob(config_dir .. "/vim/*.vim", true, true)
+for _, fpath in ipairs(vim_files) do
+    if not fpath:match("/plugins%.vim$") then
+        vim.cmd.source(fpath)
+    end
+end
+
+local lua_files = vim.fn.glob(config_dir .. "lua/*.lua", true, true)
+for _, lua_file in ipairs(lua_files) do
+    local module = vim.fs.basename(lua_file):gsub("%.lua$", "")
+    require(module)
+end
+
+local function update_spellfile(add_file)
+    local spl_file = add_file .. ".spl"
+
+    if
+        vim.fn.filereadable(spl_file) == 0
+        or vim.fn.getftime(add_file) > vim.fn.getftime(spl_file)
+    then
+        vim.cmd("silent! mkspell! " .. vim.fn.fnameescape(add_file))
+    end
+end
+
+for _, add_file in ipairs(vim.fn.glob(config_dir .. "/spell/*.add", true, true)) do
+    update_spellfile(add_file)
+end
