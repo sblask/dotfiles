a6705b66948065281891614ad51145f7336e6584
Sebastian Blask <blask@gmx.de>
2026-08-21 17:07:41 +1200

    Add IB alias

--- dotfiles_shared/.config/zsh/config.d.symlink/aliases_global.zsh
+++ dotfiles_shared/.config/zsh/config.d.symlink/aliases_global.zsh
@@ -4,8 +4,9 @@ alias -g  I="| add-index --input-type list  --print-indexables | set-index-varia
 alias -g  T="| tail --lines 25"
 alias -g BC="| awk --field-separator : '{print \$1}'" # print whatever is before the first colon
 alias -g EX="| grep -v "
-alias -g IF="| add-index --input-type mixed --print-indexables | set-index-variables"
-alias -g IT="| add-index --input-type tree  --print-indexables | set-index-variables"
+alias -g IB="| add-index --input-type git_branch --print-indexables | set-index-variables"
+alias -g IF="| add-index --input-type mixed      --print-indexables | set-index-variables"
+alias -g IT="| add-index --input-type tree       --print-indexables | set-index-variables"
 alias -g LC="| awk '{print \$NF}'" # print last column
 alias -g FC="| awk '{print \$1}'" # print first column
 
